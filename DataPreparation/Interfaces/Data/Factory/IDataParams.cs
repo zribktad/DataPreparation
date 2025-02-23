80d3d01036a4f3e43ea0aac0855ce33ddb49ef99
zribktad <zribktad@fel.cvut.cz>
2025-02-23 13:05:06 +0100

    fix for  Params

--- DataPreparation/Interfaces/Data/Factory/IDataParams.cs
+++ DataPreparation/Interfaces/Data/Factory/IDataParams.cs
@@ -4,6 +4,5 @@ namespace DataPreparation.Data.Setup;
 
 public interface IDataParams
 {
-    //Find an object of type T that satisfies the predicate
     public bool Find<T>( out T? result, Func<T, bool>? predicate = null) => throw new NotImplementedException();
 }
